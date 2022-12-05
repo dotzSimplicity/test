2a7534389d7ba48c4f45518e5b21a244611b8ab1
Simplicity <68008162+dotzSimplicity@users.noreply.github.com>
2022-12-05 16:04:25 +0000

    Comments!

--- All Code/Python/attempted_math.py
+++ All Code/Python/attempted_math.py
@@ -5,13 +5,13 @@ print("It is time to do some Math!")
 print("Note that this is Case-Sensetive.")
 print(" ")
 
-def withWhat_isnt_possible(): # This functions gets called if the withWhat variable isnt possible
+def withWhat_isnt_possible(): # This function gets called if withWhat isnt possible
     print("Either this feature isnt implemented or you wrote it wrong.")
     print("Console Calculator made by dotzSimplicity")
     print("This program has automatically exited.")
     exit()
 
-def is_withWhat_square_root(): # This function r
+def is_withWhat_square_root(): # This function gets called if withWhat is "Square Root"
     print(math.sqrt(num1))
     print("Console Calculator made by dotzSimplicity")
     exit()
@@ -20,17 +20,17 @@ possibleMath = ["Plus", "Times", "Division", "Minus", "Square Root"] # A List ne
 
 num1 = int(input("First Number Please: ")) # Gets the First Number
 
-withWhat = input("Plus, Times, Division or Minus, Square Root? ")
+withWhat = input("Plus, Times, Division or Minus, Square Root? ") # Gets the wanted method of math.
 
-if withWhat != possibleMath: #This checks if withWhat is possible
+if withWhat != possibleMath: #This checks if withWhat is possible, If so it calls a function
     withWhat_isnt_possible()
 
-if withWhat is "Square Root": #This checks if withWhat is "Square Root"
+if withWhat is "Square Root": #This checks if withWhat is "Square Root", If so it calls a function
     is_withWhat_square_root()
 
 num2 = int(input("Second Number Please: ")) # Gets the second number
 
-if withWhat == "Plus":
+if withWhat == "Plus": # If withWhat is "Plus", it adds num1 + num2
     print(num1 + num2)
 elif withWhat == "Times":
     print(num1 * num2)
@@ -40,5 +40,3 @@ elif withWhat == "Minus":
     print(num1 - num2)
 
 print("Console Calculator made by dotzSimplicity")
-
-
