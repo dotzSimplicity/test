7af56a0f8aa5afd7ba2c63f0f224eeda60175617
Simplicity <68008162+dotzSimplicity@users.noreply.github.com>
2022-12-05 19:00:58 +0000

    Manual Redirection?

--- Sites/JavaScript Test/script.js
+++ Sites/JavaScript Test/script.js
@@ -4,3 +4,10 @@ console.log("pi");
 alert("Hello World!");
 
 document.write("why yes potato man");
+
+// Make a button with an event listener
+var button = document.createElement("button");
+button.innerHTML = "Click Me";
+button.addEventListener("click", function() {
+    alert("Hello World!");
+    });
