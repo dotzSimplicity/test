f160e0cb327864b4f2ceab4ebb6e7c822f6c1c6c
Simplicity <68008162+dotzSimplicity@users.noreply.github.com>
2022-12-15 08:21:12 +0000

    robotoman is alive

--- Java/Clone man/cloner.java
+++ Java/Clone man/cloner.java
@@ -3,7 +3,7 @@ import java.util.Scanner; // needed for yoinking input
 public class cloner {
     public static void main(String[] args) {
         
-        System.out.println("hello i am robotoman, say smth bish")
+        System.out.println("hello i am robotoman, say smth bish");
         Scanner input = new Scanner(System.in); // yoinks input
         System.out.println(input.nextLine()); // yeets input
 
