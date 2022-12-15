9d7f1b346290d9860cd69b226fcb52566a52a745
Simplicity <68008162+dotzSimplicity@users.noreply.github.com>
2022-12-15 08:19:15 +0000

    potat

---
+++ Java/Clone man/cloner.java
@@ -0,0 +1,9 @@
+import java.util.Scanner; // needed for yoinking input
+
+public class cloner {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in); // yoinks input
+        System.out.println(input.nextLine()); // yeets input
+
+}}
