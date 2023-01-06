fec3777bc72dbf4442102e9ed17966cc84446d1a
Simplicity <68008162+dotzSimplicity@users.noreply.github.com>
2023-01-06 09:53:17 +0000

    honestly idk what i did

--- Sites/JavaScript Test/partymode.js
+++ Sites/JavaScript Test/partymode.js
@@ -8,12 +8,14 @@ function toggleClicked() {
         clicked = false; // not that this works you know, cause of the while loop
     } else {
         clicked = true;
-        while (clicked) {
-            rgb1++
-            rgb2+
-            rgb3++
-            document.body.style.backgroundColor = "rgb(" + rgb1 + "," + rgb2 + "," + rgb3 + ")"; // or this... cause i dont know ,copilot did this
         };
     }
 };
 
+whiile (1 == 1) {
+    while (clicked) {
+        rgb1++
+        rgb2++
+        rgb3++
+        document.body.style.backgroundColor = "rgb(" + rgb1 + "," + rgb2 + "," + rgb3 + ")"; // or this... cause i dont know ,copilot did this
+}};
